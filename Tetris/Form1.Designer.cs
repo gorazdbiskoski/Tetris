904f91ac43748df5233cdf0d2ca8009fd3fd1c32
Gorazd Biskoski <gorazdbiskoskii@gmail.com>
2024-07-06 12:47:44 +0200

    Add Final Changes to the layout and finishing touches.

--- Tetris/Form1.Designer.cs
+++ Tetris/Form1.Designer.cs
@@ -39,52 +39,60 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(460, 620);
+            this.pictureBox1.Size = new System.Drawing.Size(613, 763);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(496, 30);
+            this.lbl1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(661, 37);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(36, 13);
+            this.lbl1.Size = new System.Drawing.Size(149, 39);
             this.lbl1.TabIndex = 1;
-            this.lbl1.Text = "score:";
+            this.lbl1.Text = "SCORE: 0\r\n";
             // 
             // lbl2
             // 
             this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(496, 70);
+            this.lbl2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(662, 76);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl2.Name = "lbl2";
-            this.lbl2.Size = new System.Drawing.Size(32, 13);
+            this.lbl2.Size = new System.Drawing.Size(123, 36);
             this.lbl2.TabIndex = 2;
-            this.lbl2.Text = "level:";
+            this.lbl2.Text = "LEVEL: 0";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(527, 168);
+            this.pictureBox2.Location = new System.Drawing.Point(668, 222);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(172, 181);
+            this.pictureBox2.Size = new System.Drawing.Size(175, 176);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
             // lbl3
             // 
             this.lbl3.AutoSize = true;
-            this.lbl3.Location = new System.Drawing.Point(496, 127);
+            this.lbl3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(668, 195);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl3.Name = "lbl3";
-            this.lbl3.Size = new System.Drawing.Size(30, 13);
+            this.lbl3.Size = new System.Drawing.Size(117, 23);
             this.lbl3.TabIndex = 4;
-            this.lbl3.Text = "next:";
+            this.lbl3.Text = "NEXT SHAPE:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 662);
+            this.ClientSize = new System.Drawing.Size(861, 815);
             this.Controls.Add(this.lbl3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.lbl2);
@@ -92,6 +100,7 @@
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Tetris";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
